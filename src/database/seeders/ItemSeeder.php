f193bdb7d436bb82023b642896cc09f97fcc1e0c
Neuro <neurovoidroom@gmail.com>
2026-02-22 10:13:02 +0900

    fix: correct image URLs

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -120,7 +120,7 @@ class ItemSeeder extends Seeder
                 'price' => 2500,
                 'brand_name' => null,
                 'description' => '便利なメイクアップセット',
-                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
+                'image_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
                 'condition_id' => 2,
                 'created_at' => now(),
                 'updated_at' => now(),
